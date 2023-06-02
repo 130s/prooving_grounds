7517510e4772f8b0272700aa6760f7fb6a5927f7
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2023-06-02 12:24:35 -0400

    rclcpp_sample and child: Compile all of sudden passes

--- rclcpp_child_sample/src/exec.cpp
+++ rclcpp_child_sample/src/exec.cpp
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
   //std::shared_ptr<rclcpp_sample::DerivedModule> app(new rclcpp_sample::DerivedModule());
   auto app = std::make_shared<rclcpp_sample::DerivedModule>();
   //auto app = std::make_unique<rclcpp_sample::DerivedModule>();
+  //auto app = rclcpp_sample::DerivedModule();
 
   rclcpp::executors::MultiThreadedExecutor executor;
   executor.add_node(app);
